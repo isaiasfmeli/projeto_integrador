810dbe36a2bd1dbc028308bf5b2f5c9c8971c5c8
Isaias <isaias.finger@mercadolivre.com>
2022-08-16 20:08:07 -0300

    refactor: Customer service

--- src/main/java/br/com/dh/meli/projeto_integrador/service/ICustomerService.java
+++ src/main/java/br/com/dh/meli/projeto_integrador/service/ICustomerService.java
@@ -1,9 +1,7 @@
 package br.com.dh.meli.projeto_integrador.service;
 
-import br.com.dh.meli.projeto_integrador.dto.CustomerDTO;
 import br.com.dh.meli.projeto_integrador.model.Customer;
 
 public interface ICustomerService {
-    public Customer getCustomerById(Long id);
-    public CustomerDTO createCustomer(CustomerDTO dto);
+    Customer getCustomerById(Long id);
 }
